e72ac3f99666a6ca05762294a3ded6e3df487e4b
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-08-05 12:55:42 +0200

    change default time zone to EDT (rhc location)

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 const (
 	vkAddr          = "http://vk.com/"
 	layout          = "2006-01-02:15:04:05"
-	svgTimeLayout   = "15:04:05 -0700 EEST Jan 2 2006"
+	svgTimeLayout   = "15:04:05 -0700 EDT Jan 2 2006"
 	extData         = ".dat"
 	extGraph        = ".svg"
 	defaultPageName = "typical_enakievo"
