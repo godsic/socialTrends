5b77e0e17cb3413d55889cc13dacff04131316d7
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-08-05 12:29:01 +0200

    plot over [-500:0] minutes range

--- main.go
+++ main.go
@@ -74,7 +74,8 @@ func saveSvg(X, Y []float64, name string) {
 	p.Legend.Padding = vg.Length(5)
 	p.Y.Min = 0.0
 	p.Y.Max = 100.0
-
+	p.X.Max = 0.0
+	p.X.Min = -500.0
 	// Save the plot to a PNG file.
 	if err := p.Save(10, 5, name+extGraph); err != nil {
 		panic(err)
